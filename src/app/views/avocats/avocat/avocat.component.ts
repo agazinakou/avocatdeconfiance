03cd13c9f209f54c38060d54a7248d42ae6c2070
Lamine AZINAKOU <agazinakou@gmail.com>
2021-10-08 14:36:47 +0000

    profil update

--- src/app/views/avocats/avocat/avocat.component.ts
+++ src/app/views/avocats/avocat/avocat.component.ts
@@ -1,3 +1,4 @@
+import { Avocat } from './../../../core/models/avocat';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvocatComponent implements OnInit {
 
-  constructor() { }
+  avocat: Avocat = new Avocat();
+
+  constructor() {
+    // TODO: Retreive data from query and fetch API
+    this.avocat = {
+      id: 1,
+      picture: 'https://decizia.com/blog/wp-content/uploads/2017/06/default-placeholder.png',
+      lastname: 'Kevin',
+      firstname: 'Qwestlove',
+      law: ['Droit privé', ' Droit international'],
+      country: 'Côte d\'ivoire',
+      city: 'Abidjan',
+      pricing: '20 000 FCFA',
+      rate: '4.5'
+    };
+  }
 
   ngOnInit(): void {
   }
